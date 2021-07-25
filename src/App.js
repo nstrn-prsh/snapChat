5071775fbca220045b98d74160c315befc3f3196
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-07-25 22:56:25 +0430

    login/logout user

--- src/App.js
+++ src/App.js
@@ -2,29 +2,58 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WebcamCapture from "./components/WebcamCapture";
 import Preview from "./components/Preview";
 import Chats from "./components/Chats";
-import ChatView from './components/ChatView';
+import ChatView from "./components/ChatView";
+import { useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/appSlice";
+import Login from "./components/Login";
 import "./app.css";
+import { useEffect } from "react";
+import { auth } from "./utilise/firebase"
+import { useDispatch } from "react-redux";;
 
 function App() {
+     const user = useSelector(selectUser);
+     const dispatch = useDispatch();
+
+     useEffect(() => {
+          auth.onAuthStateChanged((authUser) => {
+               if (authUser) {
+                    dispatch(
+                         login({
+                              username: authUser.user.displayName,
+                              profilePic: authUser.user.photoURL,
+                              id: authUser.user.uid,
+                         })
+                    );
+               } else {
+                    dispatch(logout());
+               }
+          });
+     }, []);
+
      return (
           <div className='app'>
                <Router>
-                    <div className='app__body'>
-                         <Switch>
-                              <Route path='/chats/view'>
-                                   <ChatView/>
-                              </Route>
-                              <Route path='/chats'>
-                                   <Chats />
-                              </Route>
-                              <Route path='/preview'>
-                                   <Preview />
-                              </Route>
-                              <Route path='/' exact>
-                                   <WebcamCapture />
-                              </Route>
-                         </Switch>
-                    </div>
+                    {!user ? (
+                         <Login />
+                    ) : (
+                         <div className='app__body'>
+                              <Switch>
+                                   <Route path='/chats/view'>
+                                        <ChatView />
+                                   </Route>
+                                   <Route path='/chats'>
+                                        <Chats />
+                                   </Route>
+                                   <Route path='/preview'>
+                                        <Preview />
+                                   </Route>
+                                   <Route path='/' exact>
+                                        <WebcamCapture />
+                                   </Route>
+                              </Switch>
+                         </div>
+                    )}
                </Router>
           </div>
      );
---
+++ src/components/Login.js
@@ -0,0 +1,37 @@
+import { Button } from "@material-ui/core";
+import { auth, provider } from "../utilise/firebase";
+import "./login.css";
+import { useDispatch } from "react-redux";
+import { login } from "../features/appSlice";
+import snapLogo from "./../assets/pnghut_social-media-snapchat-silhouette.png";
+
+function Login() {
+     const dispatch = useDispatch();
+
+     const signIn = () => {
+          auth.signInWithPopup(provider)
+               .then((result) => {
+                    dispatch(
+                         login({
+                              username: result.user.displayName,
+                              profilePic: result.user.photoURL,
+                              id: result.user.uid,
+                         })
+                    );
+               })
+               .catch((error) => alert(error.message));
+     };
+
+     return (
+          <div className='login'>
+               <div className='login__container'>
+                    <img src={snapLogo} alt='logo' />
+                    <Button variant='outlined ' onClick={signIn}>
+                         sign in
+                    </Button>
+               </div>
+          </div>
+     );
+}
+
+export default Login;
