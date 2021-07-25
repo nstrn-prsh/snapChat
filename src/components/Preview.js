95dee5c1580c025eceba6881d57aecc9ba7ba9ca
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-07-25 14:36:07 +0430

    image: material-ui/icons and firebase

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,11 +1,25 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { selectCameraImage } from "../features/cameraSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { resetCameraImage, selectCameraImage } from "../features/cameraSlice";
 import { useHistory } from "react-router-dom";
+import { v4 as uuid } from "uuid";
+import CloseIcon from "@material-ui/icons/Close";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
+import SendIcon from "@material-ui/icons/Send";
+import "./preview.css";
+import { db, storage } from "../utilise/firebase";
+import firebase from "firebase";
 
 function Preview() {
      const cameraImage = useSelector(selectCameraImage);
-     console.log(`cameraImage: ${cameraImage}`);
+     const dispatch = useDispatch();
+
      const history = useHistory();
 
      // age axi nagerefte boodim (state ax ha khali bood) az safhe preview be safhe asli montaghel beshim
@@ -15,10 +29,69 @@ function Preview() {
           }
      }, [cameraImage, history]);
 
+     const closePreview = () => {
+          dispatch(resetCameraImage());
+     };
+
+     // FIREBASE
+     const sendPost = () => {
+          // har axi ke migirim bayad id makhsoose khodesho dashte bashe
+          const id = uuid();
+
+          // post haro toye storage firebase save mikonim :)
+          const uploadTask = storage
+               .ref(`posts/${id}`)
+               .putString(cameraImage, "data-url");
+
+          // firebase document
+          uploadTask.on(
+               "state-changed",
+               null,
+               (error) => {
+                    console.log(error);
+               },
+               // complete function
+               () => {
+                    storage
+                         .ref("posts")
+                         .child(id)
+                         .getDownloadURL()
+                         .then((url) => {
+                              db.collection("posts").add({
+                                   imageUrl: url,
+                                   username: "Nas",
+                                   read: false,
+                                   // profile pic
+                                   timestamp:
+                                        firebase.firestore.FieldValue.serverTimestamp(),
+                              });
+                              // vaghti send kardim mire to address chats
+                              history.replace("/chats");
+                         }); //end of then
+               }
+          );
+     };
+
      return (
           <div className='preview'>
-               <h1>this is your camera image</h1>
+               <CloseIcon onClick={closePreview} className='preview__close' />
+
+               <div className='preview__toolbarRight'>
+                    <TextFieldsIcon />
+                    <CreateIcon />
+                    <NoteIcon />
+                    <MusicNoteIcon />
+                    <AttachFileIcon />
+                    <CropIcon />
+                    <TimerIcon />
+               </div>
+
                <img src={cameraImage} alt='image_preview' />
+
+               <div className='preview__footer' onClick={sendPost}>
+                    <h2>send now</h2>
+                    <SendIcon size='small' className='preview__sendIcon' />
+               </div>
           </div>
      );
 }
